0ce6b7413398079f23c838f7611e8e7b000bc108
eaclumpkens <eaclumpkens@gmail.com>
2021-02-16 15:48:58 -0500

    user/review api calls

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default {
+  // Topic API
   getTopics: function() {
     return axios.get("/api/topics");
   },
@@ -12,5 +13,31 @@ export default {
   },
   saveTopic: function(topicData) {
     return axios.post("/api/topics", topicData);
+  },
+  // User API
+  getUsers: function() {
+    return axios.get("/api/users");
+  },
+  getUser: function(id) {
+    return axios.get("/api/users/" + id);
+  },
+  deleteUser: function(id) {
+    return axios.delete("/api/users/" + id);
+  },
+  saveUser: function(topicData) {
+    return axios.post("/api/users", userData);
+  },
+  // Review API
+  getReviews: function() {
+    return axios.get("/api/reviews");
+  },
+  getReview: function(id) {
+    return axios.get("/api/reviews/" + id);
+  },
+  deleteReview: function(id) {
+    return axios.delete("/api/reviews/" + id);
+  },
+  saveReview: function(topicData) {
+    return axios.post("/api/reviews", reviewData);
   }
 };
